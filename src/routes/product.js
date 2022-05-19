83bd82b22e1a60114125938ab0d5d130f427ae57
Dayanna <dayramirezvallejos@gmail.com>
2022-05-19 00:28:00 -0300

    inicio multer

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router(); 
-// const multer= require('multer');
+const multer= require('multer');
 
-// const storage= multer.diskStorage();
+const storage= multer.diskStorage({destination:(req,file,callback)=>{
+      callback(null,'public/images')
+}, filename: (req,file, callback)=>{
+      callback(null, 'prueba.png')
+}
+}); 
 
+const upload= multer({storage}); 
 
 const{detail, cart, product, gift, addProduct, productsEdit, store, search, update, remove}= require('../controllers/productController');
 router.get('/detail/:id', detail)
@@ -11,7 +17,7 @@ router.get('/detail/:id', detail)
       .get('/', product)
       .get('/gifts', gift)
       .get('/add', addProduct)
-      .post('/add', store)
+      .post('/add',upload.single('image'), store)
       .get('/edit/:id', productsEdit)
       .get('/result', search)
       .put('/update/:id', update)
